d1b81c146f42cc3ded1baa1c717e33a726f63329
menezessluciana <menezessluciana@gmail.com>
2019-11-23 13:57:05 -0300

    Create db.js

---
+++ db.js
@@ -0,0 +1,31 @@
+[
+  {
+    "userId": 1,
+    "id": 1,
+    "description": "Always",
+  },
+   {
+    "userId": 1,
+    "description": "Always",
+  },
+   {
+    "userId": 2,
+    "description": "Sometimes",
+  },
+   {
+    "userId": 3,
+    "description": "Never",
+  },
+   {
+    "userId": 4,
+    "description": "Sometimes",
+  },
+   {
+    "userId": 5,
+    "description": "Sometimes",
+  },
+   {
+    "userId": 6,
+    "description": "Always",
+  }
+ ];
